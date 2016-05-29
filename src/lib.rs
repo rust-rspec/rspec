54d6ab65104ed6dd97c65125a47f017901b9808f
Thomas Wickham <twickham@octo.com>
2016-05-29 18:33:22 +0200

    refacto tests: separate runner#run and runner#results

--- src/lib.rs
+++ src/lib.rs
@@ -108,154 +108,168 @@ mod tests {
     mod runner {
         pub use super::*;
 
-        #[test]
-        fn it_create_a_runner_that_can_be_runned() {
-            let mut runner = describe("A root", |ctx| {
-                ctx.it("is expected to run", || {
-                    assert_eq!(true, true);
-                    Ok(())
-                })
-            });
-            expect!(runner.run()).to(be_ok());
-        }
+        mod run {
+            pub use super::*;
 
-        #[test]
-        fn runner_effectively_run_tests() {
-            let ran = &mut false;
-
-            {
+            #[test]
+            fn it_create_a_runner_that_can_be_runned() {
                 let mut runner = describe("A root", |ctx| {
                     ctx.it("is expected to run", || {
-                        *ran = true;
+                        assert_eq!(true, true);
                         Ok(())
                     })
                 });
-                runner.run().unwrap()
+                expect!(runner.run()).to(be_ok());
             }
 
-            assert_eq!(true, *ran)
-        }
+            #[test]
+            fn effectively_run_tests() {
+                let ran = &mut false;
 
-        #[test]
-        fn runner_effectively_run_two_tests() {
-            use std::sync::atomic::{AtomicUsize, Ordering};
-            let ran_counter = &mut AtomicUsize::new(0);
-
-            {
-                let mut runner = describe("A root", |ctx| {
-                    ctx.it("first run",  || { ran_counter.fetch_add(1, Ordering::Relaxed); Ok(()) });
-                    ctx.it("second run", || { ran_counter.fetch_add(1, Ordering::Relaxed); Ok(()) });
-                });
-                let _ = runner.run().unwrap();
-            }
-
-            assert_eq!(2, ran_counter.load(Ordering::Relaxed))
-        }
-
-        #[test]
-        fn runner_effectively_run_two_tests_in_nested_describe() {
-            use std::sync::atomic::{AtomicUsize, Ordering};
-            let ran_counter = &mut AtomicUsize::new(0);
-
-            {
-                let mut runner = describe("A root", |ctx| {
-                    ctx.describe("first describe", |ctx| {
-                        ctx.it("first run",  || { ran_counter.fetch_add(1, Ordering::Relaxed); Ok(() )});
-                    });
-                    ctx.describe("second describe", |ctx| {
-                        ctx.it("second run",  || { ran_counter.fetch_add(1, Ordering::Relaxed); Ok(()) });
-                    });
-                    ctx.describe("third describe", |ctx| {
-                        ctx.describe("fourth describe", |ctx| {
-                            ctx.it("third run",  || { ran_counter.fetch_add(1, Ordering::Relaxed);  Ok(()) });
+                {
+                    let mut runner = describe("A root", |ctx| {
+                        ctx.it("is expected to run", || {
+                            *ran = true;
+                            Ok(())
                         })
+                    });
+                    runner.run().unwrap()
+                }
+
+                assert_eq!(true, *ran)
+            }
+
+            #[test]
+            fn effectively_run_two_tests() {
+                use std::sync::atomic::{AtomicUsize, Ordering};
+                let ran_counter = &mut AtomicUsize::new(0);
+
+                {
+                    let mut runner = describe("A root", |ctx| {
+                        ctx.it("first run",  || { ran_counter.fetch_add(1, Ordering::Relaxed); Ok(()) });
+                        ctx.it("second run", || { ran_counter.fetch_add(1, Ordering::Relaxed); Ok(()) });
+                    });
+                    let _ = runner.run().unwrap();
+                }
+
+                assert_eq!(2, ran_counter.load(Ordering::Relaxed))
+            }
+
+            #[test]
+            fn effectively_run_two_tests_in_nested_describe() {
+                use std::sync::atomic::{AtomicUsize, Ordering};
+                let ran_counter = &mut AtomicUsize::new(0);
+
+                {
+                    let mut runner = describe("A root", |ctx| {
+                        ctx.describe("first describe", |ctx| {
+                            ctx.it("first run",  || {
+                                ran_counter.fetch_add(1, Ordering::Relaxed); Ok(())
+                            });
+                        });
+                        ctx.describe("second describe", |ctx| {
+                            ctx.it("second run",  || {
+                                ran_counter.fetch_add(1, Ordering::Relaxed); Ok(())
+                            });
+                        });
+                        ctx.describe("third describe", |ctx| {
+                            ctx.describe("fourth describe", |ctx| {
+                                ctx.it("third run",  || {
+                                    ran_counter.fetch_add(1, Ordering::Relaxed); Ok(())
+                                });
+                            })
+                        })
+                    });
+                    let _ = runner.run().unwrap();
+                }
+
+                assert_eq!(3, ran_counter.load(Ordering::Relaxed))
+            }
+        }
+
+        mod results {
+            pub use super::*;
+
+            #[test]
+            fn tests_can_fail_with_an_error_result() {
+                let mut runner = describe("A root", |ctx| {
+                    ctx.it("should fail", || {
+                        Err(())
                     })
                 });
-                let _ = runner.run().unwrap();
+                runner.run().unwrap();
+
+                expect!(runner.result()).to(be_err());
             }
 
-            assert_eq!(3, ran_counter.load(Ordering::Relaxed))
-        }
+            #[test]
+            fn should_be_ok_if_tests_are_ok() {
+                let mut runner = describe("A root", |ctx| {
+                    ctx.it("should be ok", || { Ok(()) })
+                });
+                runner.run().unwrap();
 
-        #[test]
-        fn tests_can_fail_with_an_error_result() {
-            let mut runner = describe("A root", |ctx| {
-                ctx.it("should fail", || {
-                    Err(())
-                })
-            });
-            runner.run().unwrap();
+                expect!(runner.result()).to(be_ok());
+            }
 
-            expect!(runner.result()).to(be_err());
-        }
+            #[test]
+            fn is_ok_if_no_tests_have_been_runned() {
+                let mut runner = describe("A root", |_ctx| {});
+                runner.run().unwrap();
 
-        #[test]
-        fn result_should_be_ok_if_tests_are_ok() {
-            let mut runner = describe("A root", |ctx| {
-                ctx.it("should be ok", || { Ok(()) })
-            });
-            runner.run().unwrap();
+                expect!(runner.result()).to(be_ok());
+            }
 
-            expect!(runner.result()).to(be_ok());
-        }
+            #[test]
+            fn is_err_if_one_test_is_err() {
+                let mut runner = describe("A root", |ctx| {
+                    ctx.it("an err", || { Err(()) });
+                    ctx.it("an ok", || { Ok(()) });
+                });
+                runner.run().unwrap();
 
-        #[test]
-        fn results_is_ok_if_no_tests_have_been_runned() {
-            let mut runner = describe("A root", |_ctx| {});
-            runner.run().unwrap();
+                expect!(runner.result()).to(be_err());
+            }
 
-            expect!(runner.result()).to(be_ok());
-        }
+            #[test]
+            fn is_ok_if_all_tests_are_ok() {
+                let mut runner = describe("A root", |ctx| {
+                    ctx.it("ok 1", || { Ok(()) });
+                    ctx.it("ok 2", || { Ok(()) });
+                    ctx.it("ok 3", || { Ok(()) });
+                    ctx.it("ok 4", || { Ok(()) });
+                });
+                runner.run().unwrap();
 
-        #[test]
-        fn results_is_err_if_one_test_is_err() {
-            let mut runner = describe("A root", |ctx| {
-                ctx.it("an err", || { Err(()) });
-                ctx.it("an ok", || { Ok(()) });
-            });
-            runner.run().unwrap();
+                expect!(runner.result()).to(be_ok());
+            }
 
-            expect!(runner.result()).to(be_err());
-        }
+            #[test]
+            fn tests_can_contains_asserts_that_panic() {
+                use std::sync::atomic::{AtomicUsize, Ordering};
+                let counter = &mut AtomicUsize::new(0);
 
-        #[test]
-        fn results_is_ok_if_all_tests_are_ok() {
-            let mut runner = describe("A root", |ctx| {
-                ctx.it("ok 1", || { Ok(()) });
-                ctx.it("ok 2", || { Ok(()) });
-                ctx.it("ok 3", || { Ok(()) });
-                ctx.it("ok 4", || { Ok(()) });
-            });
-            runner.run().unwrap();
+                let mut runner = describe("A root", |ctx| {
+                    ctx.it("assert_eq fail", || { assert_eq!(true, false); Ok(()) });
+                    ctx.it("this should also be runned", || { counter.fetch_add(1, Ordering::Relaxed); Ok(()) })
+                });
+                runner.run().unwrap();
 
-            expect!(runner.result()).to(be_ok());
-        }
+                expect!(runner.result()).to(be_err());
+                expect!(counter.load(Ordering::Relaxed)).to(be_equal_to(1));
+            }
 
-        #[test]
-        fn tests_can_contains_asserts_that_panic() {
-            use std::sync::atomic::{AtomicUsize, Ordering};
-            let counter = &mut AtomicUsize::new(0);
+            #[test]
+            fn can_count_the_tests() {
+                let mut runner = describe("a root", |ctx| {
+                    ctx.it("first", || { Ok(()) });
+                    ctx.it("second", || { Ok(()) });
+                    ctx.it("third", || { Ok(()) });
+                });
+                runner.run().unwrap();
 
-            let mut runner = describe("A root", |ctx| {
-                ctx.it("assert_eq fail", || { assert_eq!(true, false); Ok(()) });
-                ctx.it("this should also be runned", || { counter.fetch_add(1, Ordering::Relaxed); Ok(()) })
-            });
-            runner.run().unwrap();
-
-            expect!(runner.result()).to(be_err());
-            expect!(counter.load(Ordering::Relaxed)).to(be_equal_to(1));
-        }
-
-        #[test]
-        fn can_count_the_tests() {
-            let mut runner = describe("a root", |ctx| {
-                ctx.it("first", || { Ok(()) });
-                ctx.it("second", || { Ok(()) });
-                ctx.it("third", || { Ok(()) });
-            });
-            runner.run().unwrap();
-
-            expect!(runner.result()).to(be_ok().value(TestReport { total_tests: 3 }));
+                expect!(runner.result()).to(be_ok().value(TestReport { total_tests: 3 }));
+            }
         }
     }
 
@@ -264,7 +278,7 @@ mod tests {
      * Test list:
      * x check that tests can call `assert_eq!`
      * x check that tests can return Err or Ok
-     * - runner can count the tests
+     * x runner can count the tests
      * - runner can count the success and failures
      * - check that runner displays the tests names and their results
      * - check that we can use before in a describe
