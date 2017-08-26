c3650340d62be5d36ef62bdda7260938902ce314
Vincent Esche <regexident@gmail.com>
2017-08-26 12:10:24 +0200

    Add default impl for `RunnerObserver` trait

--- src/runner/observer.rs
+++ src/runner/observer.rs
@@ -16,3 +16,13 @@ pub trait RunnerObserver: Send + Sync {
     fn enter_example(&self, example: &ExampleHeader);
     fn exit_example(&self, example: &ExampleHeader, report: &ExampleReport);
 }
+
+#[cfg(feature = "specialization")]
+default impl<T> RunnerObserver for T {
+    fn enter_suite(&self, suite: &SuiteHeader) {}
+    fn exit_suite(&self, suite: &SuiteHeader, report: &SuiteReport) {}
+    fn enter_context(&self, context: &ContextHeader) {}
+    fn exit_context(&self, context: &ContextHeader, _report: &ContextReport) {}
+    fn enter_example(&self, example: &ExampleHeader) {}
+    fn exit_example(&self, example: &ExampleHeader, report: &ExampleReport) {}
+}
