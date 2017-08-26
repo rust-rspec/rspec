e6cbc61953000d1400302b9ce74bbb0437f54899
Vincent Esche <regexident@gmail.com>
2017-08-26 17:56:23 +0200

    Change `RunnerObserver` default impls to stable syntax

--- src/runner/observer.rs
+++ src/runner/observer.rs
@@ -4,17 +4,8 @@ use header::{SuiteHeader, ContextHeader, ExampleHeader};
 use report::{SuiteReport, ContextReport, ExampleReport};
 
 /// `RunnerObserver`s can be attached to a [`Runner`](../runner/struct.Runner.html) to observe a
+#[allow(unused_variables)]
 pub trait RunnerObserver: Send + Sync {
-    fn enter_suite(&self, header: &SuiteHeader);
-    fn exit_suite(&self, header: &SuiteHeader, report: &SuiteReport);
-    fn enter_context(&self, header: &ContextHeader);
-    fn exit_context(&self, header: &ContextHeader, report: &ContextReport);
-    fn enter_example(&self, header: &ExampleHeader);
-    fn exit_example(&self, header: &ExampleHeader, report: &ExampleReport);
-}
-
-#[cfg(feature = "specialization")]
-default impl<T> RunnerObserver for T {
     fn enter_suite(&self, header: &SuiteHeader) {}
     fn exit_suite(&self, header: &SuiteHeader, report: &SuiteReport) {}
     fn enter_context(&self, header: &ContextHeader) {}
