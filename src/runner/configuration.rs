2b2eca382c33861e681ae5901fad97f91b685642
Thomas Wickham <twickham@octo.com>
2017-08-27 16:24:11 +0200

    impl default for configuration runner

--- src/runner/configuration.rs
+++ src/runner/configuration.rs
@@ -1,6 +1,8 @@
 // derive_builder emits warnings otherwise:
 #![allow(unused_mut)]
 
+use std::default::Default;
+
 /// A Runner's configuration.
 #[derive(Builder)]
 pub struct Configuration {
@@ -12,15 +14,32 @@ pub struct Configuration {
     pub exit_on_failure: bool,
 }
 
+impl Default for Configuration {
+    fn default() -> Self {
+        ConfigurationBuilder::default().build().unwrap()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn default() {
+    fn default_with_builder() {
         let config = ConfigurationBuilder::default().build().unwrap();
         assert_eq!(config.parallel, true);
         assert_eq!(config.exit_on_failure, true);
+    }
+
+    #[test]
+    fn default() {
+        // arrange
+        let expected = ConfigurationBuilder::default().build().unwrap();
+        // act
+        let config = Configuration::default();
+        // assert
+        assert_eq!(expected.parallel, config.parallel);
+        assert_eq!(expected.exit_on_failure, config.exit_on_failure);
     }
 
     #[test]
