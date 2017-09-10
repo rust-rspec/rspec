49777436193a03a6f7dc2d361ea2b2f905a270c4
Thomas Wickham <twickham@octo.com>
2017-09-10 11:33:16 +0200

    remove dead code

--- src/logger/serial.rs
+++ src/logger/serial.rs
@@ -255,264 +255,31 @@ where
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     pub use super::*;
-//     pub use runner::observer::{Event, RunnerObserver};
-//     pub use example_report::*;
-//     pub use std::io;
-//     pub use std::str;
-//
-//     #[test]
-//     fn it_can_be_instanciated() {
-//         SerialLogger::new(&mut vec![1u8]);
-//     }
-//
-//     #[test]
-//     fn it_impl_logger_trait() {
-//         let _: &SerialLogger = &SerialLogger::new(&mut vec![1u8]) as &SerialLogger;
-//     }
-//
-//     mod event_start_runner {
-//         pub use super::*;
-//
-//         #[test]
-//         fn it_display_that_examples_started() {
-//             let mut v = vec![];
-//             {
-//                 let mut s = SerialLogger::new(&mut v);
-//                 s.handle(&Event::EnterSuite);
-//             }
-//
-//             assert_eq!("\ntests", str::from_utf8(&v).unwrap());
-//         }
-//     }
-//
-//     mod event_finished_runner {
-//         pub use super::*;
-//         use runner::ContextReport;
-//
-//         macro_rules! test_and_compare_output {
-//             ($(
-//                 $test_name:ident : (passed: $succ:expr, failed: $fail:expr) => $msg:expr
-//             ),+) => {
-//
-//                 $(
-//                     #[test]
-//                     fn $test_name() {
-//                         let mut sink = io::sink();
-//                         let res = {
-//                             let mut s = SerialLogger::new(&mut sink);
-//                             s.write_summary(ContextReport {
-//                                 passed: $succ,
-//                                 failed: $fail,
-//                                 ignored: 0,
-//                                 measured: 0,
-//                             })
-//                         };
-//
-//                         assert_eq!($msg, res)
-//                     }
-//                 )+
-//             }
-//         }
-//
-//         test_and_compare_output! {
-//             no_example_is_ok: (passed: 0, failed: 0) =>
-//                 "\n\ntest result: ok. 0 examples; 0 passed; 0 failed;",
-//             one_example: (passed: 1, failed: 0) =>
-//                 "\n\ntest result: ok. 1 examples; 1 passed; 0 failed;",
-//             multiple_ok: (passed: 42, failed: 0) =>
-//                 "\n\ntest result: ok. 42 examples; 42 passed; 0 failed;",
-//             one_error: (passed: 0, failed: 1) =>
-//               "\n\ntest result: FAILED. 1 examples; 0 passed; 1 failed;",
-//             multiple_errors: (passed: 0, failed: 37) =>
-//               "\n\ntest result: FAILED. 37 examples; 0 passed; 37 failed;",
-//             one_of_each: (passed: 1, failed: 1) =>
-//               "\n\ntest result: FAILED. 2 examples; 1 passed; 1 failed;",
-//             multiple_of_each: (passed: 12, failed: 21) =>
-//               "\n\ntest result: FAILED. 33 examples; 12 passed; 21 failed;"
-//         }
-//     }
-//
-//     mod event_end_example {
-//         pub use super::*;
-//
-//         #[test]
-//         fn it_displays_a_dot_when_success() {
-//             let mut v = vec![];
-//             {
-//                 let mut s = SerialLogger::new(&mut v);
-//                 s.handle(&Event::ExitExample(SUCCESS_RES))
-//             }
-//
-//             assert_eq!(".", str::from_utf8(&v).unwrap());
-//         }
-//
-//         #[test]
-//         #[allow(non_snake_case)]
-//         fn it_displays_a_F_when_error() {
-//             let mut v = vec![];
-//             {
-//                 let mut s = SerialLogger::new(&mut v);
-//                 s.handle(&Event::ExitExample(FAILED_RES))
-//             }
-//
-//             assert_eq!("F", str::from_utf8(&v).unwrap());
-//         }
-//     }
-//
-//     mod event_start_end_describe {
-//         pub use super::*;
-//
-//         #[test]
-//         fn start_describe_event_push_the_name_stack() {
-//             let mut sink = &mut io::sink();
-//             let mut s = SerialLogger::new(&mut sink);
-//
-//             s.handle(&Event::EnterContext(String::from("Hey !")));
-//             assert_eq!(vec![String::from("Hey !")], s.name_stack);
-//
-//             s.handle(&Event::EnterContext(String::from("Ho !")));
-//             assert_eq!(vec![String::from("Hey !"), String::from("Ho !")],
-//                        s.name_stack)
-//         }
-//
-//         #[test]
-//         fn end_describe_event_pop_the_name_stack() {
-//             let mut sink = &mut io::sink();
-//             let mut s = SerialLogger::new(&mut sink);
-//
-//             s.handle(&Event::EnterContext(String::from("Hey !")));
-//             s.handle(&Event::EnterContext(String::from("Ho !")));
-//
-//             s.handle(&Event::ExitContext);
-//             assert_eq!(vec![String::from("Hey !")], s.name_stack);
-//
-//             s.handle(&Event::ExitContext);
-//             assert_eq!(0, s.name_stack.len());
-//         }
-//     }
-//
-//     mod failures_pretty_printing {
-//         use super::*;
-//
-//         #[test]
-//         fn it_register_failures() {
-//             let mut sink = &mut io::sink();
-//             let mut s = SerialLogger::new(&mut sink);
-//             s.handle(&Event::EnterExample("hola".into()));
-//             s.handle(&Event::ExitExample(FAILED_RES));
-//             assert_eq!(1, s.failed.len());
-//         }
-//
-//         #[test]
-//         fn it_keep_track_of_the_failure_name() {
-//             let mut sink = &mut io::sink();
-//             let mut s = SerialLogger::new(&mut sink);
-//             s.handle(&Event::EnterExample("hola".into()));
-//             s.handle(&Event::ExitExample(FAILED_RES));
-//             assert_eq!(Some(&"hola".into()), s.failed.get(0));
-//         }
-//
-//         #[test]
-//         fn it_has_a_nice_diplay_for_describes() {
-//             let mut sink = &mut io::sink();
-//             let mut s = SerialLogger::new(&mut sink);
-//             s.handle(&Event::EnterContext("hola".into()));
-//             s.handle(&Event::EnterExample("holé".into()));
-//             s.handle(&Event::ExitExample(FAILED_RES));
-//             assert_eq!(Some(&"hola | holé".into()), s.failed.get(0));
-//
-//             s.handle(&Event::EnterContext("ohééé".into()));
-//             s.handle(&Event::EnterExample("holé".into()));
-//             s.handle(&Event::ExitExample(FAILED_RES));
-//             assert_eq!(Some(&"hola | ohééé | holé".into()), s.failed.get(1));
-//         }
-//
-//         #[test]
-//         fn it_works_with_multiple_describes() {
-//             let mut sink = &mut io::sink();
-//             let mut s = SerialLogger::new(&mut sink);
-//             s.handle(&Event::EnterContext("hola".into()));
-//             s.handle(&Event::EnterExample("holé".into()));
-//             s.handle(&Event::ExitExample(FAILED_RES));
-//
-//             s.handle(&Event::ExitContext);
-//             s.handle(&Event::EnterContext("ok".into()));
-//             s.handle(&Event::EnterExample("cacao".into()));
-//             s.handle(&Event::ExitExample(FAILED_RES));
-//             assert_eq!(Some(&"ok | cacao".into()), s.failed.get(1));
-//         }
-//
-//         #[test]
-//         fn it_doesnt_includes_success() {
-//             let mut sink = &mut io::sink();
-//             let mut s = SerialLogger::new(&mut sink);
-//             s.handle(&Event::EnterContext("hola".into()));
-//             s.handle(&Event::EnterExample("holé".into()));
-//             s.handle(&Event::ExitExample(SUCCESS_RES));
-//
-//             assert_eq!(None, s.failed.get(0));
-//         }
-//
-//         #[test]
-//         fn is_doesnt_keep_examples_in_name_stack() {
-//             let mut sink = &mut io::sink();
-//             let mut s = SerialLogger::new(&mut sink);
-//             s.handle(&Event::EnterContext("hola".into()));
-//             s.handle(&Event::EnterExample("holé".into()));
-//             s.handle(&Event::ExitExample(SUCCESS_RES));
-//             s.handle(&Event::EnterExample("holé".into()));
-//             s.handle(&Event::ExitExample(FAILED_RES));
-//
-//             // not "hola | holé | holé"
-//             assert_eq!(Some(&"hola | holé".into()), s.failed.get(0));
-//         }
-//
-//         #[test]
-//         fn format_all_failures_one_error() {
-//             let mut sink = &mut io::sink();
-//             let res = {
-//                 let mut s = SerialLogger::new(&mut sink);
-//                 s.handle(&Event::EnterContext("hola".into()));
-//                 s.handle(&Event::EnterExample("holé".into()));
-//                 s.handle(&Event::ExitExample(FAILED_RES));
-//                 s.failures_summary()
-//             };
-//
-//             assert_eq!("  1) hola | holé\n", res);
-//         }
-//
-//         #[test]
-//         fn format_all_failures() {
-//             let mut sink = &mut io::sink();
-//             let res = {
-//                 let mut s = SerialLogger::new(&mut sink);
-//                 s.handle(&Event::EnterContext("hola".into()));
-//                 s.handle(&Event::EnterExample("holé".into()));
-//                 s.handle(&Event::ExitExample(FAILED_RES));
-//                 s.handle(&Event::EnterExample("hola".into()));
-//                 s.handle(&Event::ExitExample(FAILED_RES));
-//                 s.failures_summary()
-//             };
-//
-//             assert_eq!("  1) hola | holé\n  2) hola | hola\n", res);
-//
-//             let res = {
-//                 let mut s = SerialLogger::new(&mut sink);
-//                 s.handle(&Event::EnterContext("hola".into()));
-//                 s.handle(&Event::EnterExample("holé".into()));
-//                 s.handle(&Event::ExitExample(FAILED_RES));
-//                 s.handle(&Event::ExitContext);
-//                 s.handle(&Event::EnterContext("second".into()));
-//                 s.handle(&Event::EnterContext("third".into()));
-//                 s.handle(&Event::EnterExample("hola".into()));
-//                 s.handle(&Event::ExitExample(FAILED_RES));
-//                 s.failures_summary()
-//             };
-//
-//             assert_eq!("  1) hola | holé\n  2) second | third | hola\n", res);
-//         }
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod padding {
+        use super::*;
+
+        #[test]
+        fn it_padds() {
+            // arrange
+            let expected = vec![
+                ("", 0),
+                ("  ", 1),
+                ("     ", 2),
+                ("       ", 3)
+            ];
+            for (expected_res, given_depth) in expected {
+                // act
+                let res = SerialLogger::<Vec<u8>>::padding(given_depth);
+                // assert
+                assert_eq!(String::from(expected_res), res)
+            }
+        }
+    }
+
+
+}
+
